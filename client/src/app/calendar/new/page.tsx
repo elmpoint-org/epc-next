e153434cfc8185be57a002cb18b032a62d867cf3
Michael Foster <hingobway@users.noreply.github.com>
2024-06-17 17:57:37 -0400

    Make calendar protected (#41)

--- client/src/app/calendar/new/page.tsx
+++ client/src/app/calendar/new/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 
 import NewEventForm from './_components/Form';
+import LoginBoundary from '@/app/_components/_base/LoginBoundary/LoginBoundary';
 
 export const metadata: Metadata = {
   title: 'add your stay',
@@ -9,11 +10,13 @@ export const metadata: Metadata = {
 export default function CalendarNewPage() {
   return (
     <>
-      <div className="flex-1 rounded-lg bg-slate-100">
-        <h2 className="p-6 text-center text-2xl">Add your stay</h2>
+      <LoginBoundary>
+        <div className="flex-1 rounded-lg bg-slate-100">
+          <h2 className="p-6 text-center text-2xl">Add your stay</h2>
 
-        <NewEventForm />
-      </div>
+          <NewEventForm />
+        </div>
+      </LoginBoundary>
     </>
   );
 }
