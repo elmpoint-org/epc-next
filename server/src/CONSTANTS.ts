dbe9da42e7f7423294dd00383e44c42d0f722151
Michael Foster <hingobway@users.noreply.github.com>
2024-06-19 23:20:37 -0400

    Update login expiration time (#56)

--- server/src/CONSTANTS.ts
+++ server/src/CONSTANTS.ts
@@ -4,6 +4,6 @@ export const DEFAULT_KEY_LENGTH = 32;
 export const JWT_ALGORITHM = 'HS256';
 
 // prettier-ignore
-export const LOGIN_EXPIRE = (24) * 3600; // (#) hours
+export const LOGIN_EXPIRE = (6) * 2629800 // (#) months
 // prettier-ignore
 export const EMAIL_LOGIN_EXPIRE = (6) * 3600 // (#) hours
