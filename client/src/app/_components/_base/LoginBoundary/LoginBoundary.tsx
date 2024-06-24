868a22a5c27267592374b39c76d4e57e4e8c3c2e
hingobway <hingobway@users.noreply.github.com>
2024-06-24 18:15:58 -0400

    allow scoped pages

--- client/src/app/_components/_base/LoginBoundary/LoginBoundary.tsx
+++ client/src/app/_components/_base/LoginBoundary/LoginBoundary.tsx
@@ -1,12 +1,23 @@
 import { Children } from '@/util/propTypes';
-import { getUser } from '@/app/_ctx/user/provider';
+import { AuthUser, getUser } from '@/app/_ctx/user/provider';
 import LoginBoundaryRedirect from './LoginBoundaryRedirect';
+import ScopeError from './ScopeError';
 
 /** redirect all users who aren't logged in */
-export default async function LoginBoundary({ children }: Children) {
+export default async function LoginBoundary({
+  scope,
+  children,
+}: { scope?: AuthUser['scope'] & {} } & Children) {
   const user = await getUser();
 
   if (!user) return <LoginBoundaryRedirect />;
+
+  // check scope if requested
+  scope?.push('ADMIN');
+  const isAllowed = !scope
+    ? true
+    : scope.length > scope.filter((s) => !user.scope?.includes(s)).length;
+  if (!isAllowed) return <ScopeError />;
 
   return <>{children}</>;
 }
