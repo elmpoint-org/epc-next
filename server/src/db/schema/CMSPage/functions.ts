bb895135a4bf9bc3a323524423bd363842dd8fa0
hingobway <hingobway@users.noreply.github.com>
2024-06-25 14:43:56 -0400

    db code cleanup

--- server/src/db/schema/CMSPage/functions.ts
+++ server/src/db/schema/CMSPage/functions.ts
@@ -1,42 +1,38 @@
-import {
-  CmsPageResolvers,
-  MutationResolvers,
-  QueryResolvers,
-} from '@@/db/__types/graphql-types';
-import { ResolverContext } from '@@/db/graph';
+import type { CmsPageModule as M } from './__types/module-types';
 import {
   err,
   getTypedScopeFunctions,
   handle as h,
   scopeError,
 } from '@@/db/lib/utilities';
-import { DBCmsPage } from './source';
 import { DBType } from '@@/db/lib/Model';
+import { DBCmsPage } from './source';
 
+import { ResolverContext } from '@@/db/graph';
 const { scoped, scopeDiff } = getTypedScopeFunctions<ResolverContext>();
 
-export const getCmsPages = h<QueryResolvers['cmsPages']>(
+export const getCmsPages = h<M.QueryResolvers['cmsPages']>(
   async ({ sources, userId }) => {
     if (!userId) throw scopeError();
     return sources.cms.page.getAll();
   }
 );
 
-export const getCmsPage = h<QueryResolvers['cmsPage']>(
+export const getCmsPage = h<M.QueryResolvers['cmsPage']>(
   async ({ sources, args: { id }, userId }) => {
     if (!userId) throw scopeError();
     return sources.cms.page.get(id);
   }
 );
 
-export const getCmsPageFromSlug = h<QueryResolvers['cmsPageFromSlug']>(
+export const getCmsPageFromSlug = h<M.QueryResolvers['cmsPageFromSlug']>(
   async ({ sources, args: { slug } }) => {
     const q = await sources.cms.page.findBy('slug', slug);
     return q?.[0] ?? null;
   }
 );
 
-export const cmsPageCreate = h<MutationResolvers['cmsPageCreate']>(
+export const cmsPageCreate = h<M.MutationResolvers['cmsPageCreate']>(
   scoped('ADMIN', 'EDIT'),
   async ({ sources, args: { contributorAdd, ...pageParams } }) => {
     const page = pageParams as DBCmsPage;
@@ -58,7 +54,7 @@ export const cmsPageCreate = h<MutationResolvers['cmsPageCreate']>(
   }
 );
 
-export const cmsPageUpdate = h<MutationResolvers['cmsPageUpdate']>(
+export const cmsPageUpdate = h<M.MutationResolvers['cmsPageUpdate']>(
   scoped('ADMIN', 'EDIT'),
   async ({
     sources,
@@ -87,7 +83,7 @@ export const cmsPageUpdate = h<MutationResolvers['cmsPageUpdate']>(
   }
 );
 
-export const cmsPageDelete = h<MutationResolvers['cmsPageDelete']>(
+export const cmsPageDelete = h<M.MutationResolvers['cmsPageDelete']>(
   scoped('ADMIN', 'EDIT'),
   async ({ sources, args: { id }, userId, scope }) => {
     const p = await sources.cms.page.get(id);
@@ -100,7 +96,7 @@ export const cmsPageDelete = h<MutationResolvers['cmsPageDelete']>(
   }
 );
 
-export const getCmsPageContributors = h<CmsPageResolvers['contributors']>(
+export const getCmsPageContributors = h<M.CMSPageResolvers['contributors']>(
   async ({ sources, parent }) => {
     const { contributorIds } = parent as DBType<DBCmsPage>;
 
