f103914429f7f9b8bec06bb5f615d9ab9bdfe084
hingobway <hingobway@users.noreply.github.com>
2024-07-25 23:08:04 -0400

    fix datestats + improved descriptions

--- client/src/app/calendar/new/_components/DateStats.tsx
+++ client/src/app/calendar/new/_components/DateStats.tsx
@@ -4,19 +4,29 @@ import dayjs from 'dayjs';
 import { COST_GUESTS, COST_MEMBERS } from './Form';
 import { useFormCtx } from '../state/formCtx';
 
+const DAYS_MAX = 150;
+
 const DateStats = () => {
   const { dates } = useFormCtx();
 
   const diff = useMemo(() => {
-    const d = dayjs(dates[1]).diff(dayjs(dates[0]), 'days') + 1;
-    return Number.isFinite(d) ? Math.abs(d) : 0;
+    const d = dayjs(dates[1]).diff(dayjs(dates[0]), 'days');
+    if (!Number.isFinite(d)) return 0;
+    return Math.abs(d);
   }, [dates]);
+  const diffLim = useMemo(() => Math.min(diff, DAYS_MAX), [diff]);
+  const isMax = diff > diffLim;
+
+  const memberCost = useMemo(() => diffLim * COST_MEMBERS, [diffLim]);
+  const guestCost = useMemo(() => diffLim * COST_GUESTS, [diffLim]);
 
   return (
     <>
       <div className="flex flex-row justify-end pb-4">
         <p className="text-sm text-slate-600">
-          {`${diff} day${diff !== 1 ? 's':''} - \$${diff * COST_MEMBERS}/member - \$${diff * COST_GUESTS}/guest`}
+          {`${diffLim}${isMax ? '+' : ''} night${diffLim !== 1 ? 's' : ''} - `}
+          {!isMax && `\$${memberCost}/member - \$${guestCost}/guest`}
+          {isMax && `$???/member - $???/guest`}
         </p>
       </div>
     </>
