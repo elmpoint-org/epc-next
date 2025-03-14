0b6c278e5366953757765db33ec01d369d51366a
Michael Foster <hingobway@users.noreply.github.com>
2025-03-14 00:07:48 -0400

    Nav fixes (#196)

--- client/src/app/calendar/page.tsx
+++ client/src/app/calendar/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function CalendarPage() {
   return (
     <>
-      <div className="flex-1 rounded-lg bg-slate-100">
+      <div className="page-full-width flex-1 rounded-lg bg-slate-100">
         <div className="mx-auto flex min-h-dvh flex-col gap-4 p-6">
           <Calendar />
 
