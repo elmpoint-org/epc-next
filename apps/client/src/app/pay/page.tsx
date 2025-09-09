56dd0dcc0682100c0d127774514c3b4f9a24e2ea
hingobway <hingobway@users.noreply.github.com>
2025-09-09 00:06:05 -0400

    add first payments table

--- apps/client/src/app/pay/page.tsx
+++ apps/client/src/app/pay/page.tsx
@@ -1,10 +1,12 @@
 import { Metadata } from 'next';
 import ColorText from '../_components/_base/ColorText';
 import {
+  IconChevronRight,
   IconCodeVariablePlus,
   IconHomeDollar,
   IconPigMoney,
 } from '@tabler/icons-react';
+import { Button, Checkbox } from '@mantine/core';
 
 export const metadata: Metadata = {
   title: 'Payment Portal',
@@ -28,10 +30,9 @@ export default function Page() {
               </p>
             </div>
 
-            <div className="relative flex flex-col gap-2 rounded-lg border border-slate-200 p-4 text-sm shadow-sm">
+            <div className="relative flex flex-col gap-2 rounded-lg border border-slate-200 p-6 text-sm shadow-sm">
               {/* new payment options */}
-
-              <p className="pb-2 text-base">Select payment type:</p>
+              <h3 className="pb-2 text-base">Make a new payment&hellip;</h3>
               <div className="flex flex-row gap-4 rounded-sm">
                 <button className="flex flex-1 cursor-pointer flex-col rounded-md border border-slate-300 p-3.5 text-left text-slate-700 shadow-sm shadow-slate-600/10 transition hover:border-slate-400">
                   <div className="mb-2 flex flex-row items-center gap-2">
@@ -64,6 +65,61 @@ export default function Page() {
                   <p className="t">Fundraisers, custom payments, etc.</p>
                 </button>
               </div>
+
+              <hr className="my-4 border-slate-300" />
+
+              {/* ready payments table */}
+
+              <h3 className="text-base">Ready to pay</h3>
+
+              <table
+                className="grid grid-flow-col gap-x-4 text-left"
+                style={{
+                  gridTemplateColumns:
+                    'min-content min-content 1fr min-content min-content',
+                }}
+              >
+                <thead className="col-span-full grid grid-cols-subgrid border-b border-slate-300 px-2 py-2">
+                  <tr className="col-span-full grid grid-cols-subgrid items-center text-nowrap">
+                    <th>
+                      <Checkbox size="xs" />
+                    </th>
+                    <th>Type</th>
+                    <th>Description</th>
+                    <th className="text-center sm:w-20">Amount</th>
+                    <th className="">
+                      <Button size="compact-sm" className="px-3">
+                        Pay all
+                      </Button>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="col-span-full grid grid-cols-subgrid divide-y divide-slate-200 *:p-2">
+                  {Array.from({ length: 2 }).map((_, i) => (
+                    <tr
+                      key={i}
+                      className="col-span-full grid grid-cols-subgrid items-center"
+                    >
+                      <td>
+                        <Checkbox size="xs" />
+                      </td>
+                      <td className="text-nowrap">Head Tax</td>
+                      <td>Dec 20-27, 2024 (7 nights, 3 members)</td>
+                      <td className="text-center font-bold">$300</td>
+                      <td>
+                        <Button
+                          size="compact-sm"
+                          variant="subtle"
+                          className="group w-full"
+                        >
+                          Pay{' '}
+                          <IconChevronRight className="-mb-0.5 ml-0 inline size-4 transition-all group-hover:ml-0.5" />
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
