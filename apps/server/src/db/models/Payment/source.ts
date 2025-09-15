f5311a0c8439aee3370279a161bc05db89470aed
hingobway <hingobway@users.noreply.github.com>
2025-09-14 23:02:38 -0400

    payment type scaffolding

--- apps/server/src/db/models/Payment/source.ts
+++ apps/server/src/db/models/Payment/source.ts
@@ -1,10 +1,13 @@
 import Model from '##/db/lib/Model.js';
 import { Payment } from '##/db/__types/graphql-types.js';
 
-export type DBPayment = {} & Payment;
+export type DBPayment = {
+  userId: string;
+  stripeTransactionId?: string;
+} & Payment;
 
 class PaymentSource extends Model<DBPayment> {
-  protected table = '';
-  protected type = '';
+  protected table = 'epc--payment';
+  protected type = 'payment';
 }
 export default PaymentSource;
--- apps/server/src/db/models/Payment/types.graphql.ts
+++ apps/server/src/db/models/Payment/types.graphql.ts
@@ -1,9 +1,64 @@
 import gql from 'graphql-tag';
 
 export default gql`
+  "A singular payment from user to EPC."
   type Payment {
     id: ID!
 
+    "the user making or intending to make this payment"
+    user: User
+
+    "the total dollar amount (USD)"
+    amount: Int!
+
+    "payment status. if set to STRIPE, check the stripe status instead."
+    status: PaymentStatus!
+    details: PaymentDetails!
+
+    "optional: a stripe transaction, iff user chooses to pay with stripe"
+    stripeTransaction: StripePaymentIntent
+
     timestamp: TS!
+  }
+
+  "a daily residence payment"
+  type PaymentDetails_HeadTax {
+    "the calendar reservation being paid for"
+    stay: Stay
+  }
+  type PaymentDetails_AnnualDues {
+    shares: Int!
+  }
+  type PaymentDetails_DayTripper {
+    dayTripper: Boolean!
+  }
+  type PaymentDetails_Generic {
+    description: String!
+  }
+
+  # ------------------------------------
+
+  type Query {
+    payment: Payment
+  }
+
+  # ------------------------------------
+
+  union PaymentDetails =
+      PaymentDetails_HeadTax
+    | PaymentDetails_AnnualDues
+    | PaymentDetails_DayTripper
+    | PaymentDetails_Generic
+
+  enum PaymentStatus {
+    UNPAID
+    PAYMENT_SENT
+    PAID
+    STRIPE
+  }
+
+  type StripePaymentIntent {
+    id: ID!
+    # TODO
   }
 `;
